7dd17a98ed9f36668e089e51e486bb1a728fc3dd
Teun de Clercq <t.declercq@student.fontys.nl>
2020-01-12 13:03:06 +0100

    Tests of domain model done

--- src/test/java/nl/fontys/tictactoe/Models/PlayerTest.java
+++ src/test/java/nl/fontys/tictactoe/Models/PlayerTest.java
@@ -1,6 +1,5 @@
 package nl.fontys.tictactoe.Models;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,48 +7,77 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class PlayerTest {
-
+    Player player;
     @BeforeEach
     void setUp() {
+        player = new Player("UdPrf1cFM2XdNtu12", "teundeclercq@gmail.com", true, Button.X, Button.X);
     }
 
     @Test
     void getId() {
+        String id = "UdPrf1cFM2XdNtu12";
+        assertEquals(id, player.getId());
     }
-
+    @Test
+    void emptyConstructor() {
+        player = new Player();
+        assertNull(player.getId());
+        assertNull(player.getPlayerFiguur());
+    }
+    @Test
+    void constructorWithIdEmailCanMove() {
+        player = new Player("UdPrf1cFM2XdNtu12", "teundeclercq@gmail.com", true);
+        assertNull(player.getPlayerTurn());
+        assertNull(player.getPlayerFiguur());
+    }
     @Test
     void getEmail() {
+        String email = "teundeclercq@gmail.com";
+        assertEquals(email, player.getEmail());
     }
 
     @Test
     void getCanMove() {
+        assertEquals(true, player.getCanMove());
     }
 
     @Test
     void getPlayerFiguur() {
+        assertEquals(Button.X, player.getPlayerFiguur());
     }
 
     @Test
     void getPlayerTurn() {
+        assertEquals(Button.X, player.getPlayerFiguur());
     }
 
     @Test
     void setId() {
+        player.setId("IUY");
+        assertEquals("IUY", player.getId());
     }
 
     @Test
     void setEmail() {
+        player.setEmail("anneschroen@gmail.com");
+        assertEquals("anneschroen@gmail.com", player.getEmail());
     }
 
     @Test
     void setCanMove() {
+        player.setCanMove(false);
+        assertEquals(false, player.getCanMove());
     }
 
     @Test
     void setPlayerFiguur() {
+        player.setPlayerFiguur(Button.O);
+        assertEquals(Button.O, player.getPlayerFiguur());
     }
 
     @Test
     void setPlayerTurn() {
+        player.setPlayerTurn(Button.O);
+        assertEquals(Button.O, player.getPlayerTurn());
     }
 }
